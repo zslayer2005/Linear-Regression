e595f2b442bd63a3f93c3d0dcf40c0bb07f2f893
Aryan sharma <aryansharma231849@gmail.com>
2025-06-27 14:18:31 +0530

    Create Task3_LinearRegression.py

---
+++ Task3_LinearRegression.py
@@ -0,0 +1,26 @@
+
+# Task 3: Linear Regression (Python Script Version)
+
+
+
+
+
+
+This project performs simple and multiple linear regression on a housing dataset.
+## Steps:
+
+- Import libraries
+- Load and preprocess dataset
+- Perform simple and multiple linear regression
+- Evaluate the models (MAE, MSE, R2)
+- Plot results
+
+ ## Requirements
+
+Run pip install -r requirements.txt to install all dependencies.
+
+ ## How to Run
+Make sure you have a dataset file HousingData.csv inside a dataset/ folder. Then run:
+python Task3_LinearRegression.py
+
+
